dc75176db961457f4cd9678b5f60166bcdd41862
pranushas9071 <pranusha437@gmail.com>
2021-08-07 10:13:55 +0530

    Refracted

--- Routers/birds.js
+++ Routers/birds.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 let router = express.Router();
 router.get("/", (req, res) => {
   res.send("Home page...");
@@ -6,5 +7,6 @@ router.get("/", (req, res) => {
 router.get("/about", (req, res) => {
   res.send("About birds...");
 });
+
 // router.use() =>router middleware.....
 export default router;
