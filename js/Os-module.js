4a7c7a065e22ee71ddae62fbf1ade1f5cca4e7cb
pranushas9071 <pranusha437@gmail.com>
2021-08-09 09:59:52 +0530

    Revised

--- js/Os-module.js
+++ js/Os-module.js
@@ -8,7 +8,6 @@ console.log(`loadavg : ${os.loadavg()}`); // meaningful value only in linux and 
 console.log(`nteworkInterfaces : ${os.networkInterfaces()}`); // network -- wifi....
 console.log(`platform : ${os.platform()}`); // in which node.js has been compiled--> win32
 console.log(`release : ${os.release()}`); //release of od
-console.log(`tmpdir : ${os.tmpdir()}`); // ??
 console.log(`totalmem : ${os.totalmem()}`); // no of bytes in total memory
 console.log(`type : ${os.type()}`); // linux or windows...
 console.log(`uptime : ${os.uptime()}`); // no of seconds that the system is running from the last reboot
